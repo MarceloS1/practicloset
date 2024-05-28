6dc39f56aeabca447041a3438ce39f871459ea12
Marcelo <a2020116103@estudiantes.upsa.edu.bo>
2024-05-28 02:02:37 -0400

    stockRoutes actualizado

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const stockController = require('../controllers/stockController');
 
-router.put('/:producto_id', stockController.actualizarStock);
-router.delete('/:producto_id', stockController.eliminarStock);
+router.put('/modelo/:modelo_id', stockController.actualizarStock);
+router.put('/articulo/:articulo_id', stockController.actualizarStock);
+router.delete('/modelo/:modelo_id', stockController.eliminarStock);
+router.delete('/articulo/:articulo_id', stockController.eliminarStock);
 
 module.exports = router;
