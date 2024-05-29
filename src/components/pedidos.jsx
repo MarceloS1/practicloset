b1d40b52e47e8cbd6fa89208fe8d74f6f5333b4c
a2020211131 <a2020211131@estudiantes.upsa.edu.bo>
2024-05-28 21:30:54 -0400

    actualizacion del observer de Mache

--- src/components/pedidos.jsx
+++ src/components/pedidos.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const baseUrl = 'http://25.56.40.70:5000';
+const baseUrl = 'http://25.41.163.224:5000';
 
 const Pedidos = () => {
     const [pedidos, setPedidos] = useState([]);
