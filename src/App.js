392e6080c416e749f1379e301452452a2b23c919
a2020211131 <a2020211131@estudiantes.upsa.edu.bo>
2024-05-29 00:07:36 -0400

    ahora tiene pagina de Inicio. falta hacerlo responsive, lo vamos a hacer responsive? perdi el mental hay que ver si mañana sigue funcionando

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ import OrdenesDeCompra from './components/ordenesdecompra';
 import FormularioTrabajador from './components/trabajadores';
 import GestionClientes from './components/clientes';
 import GestionStock from './components/stock';
-import '../src/css/base.css';
+import './css/base.css';
 import './App.css';
 import FormularioModelo from './components/modelos';
 import OrdenesCompra from './components/ordenesdecompra';
 import GestionPedidosYPagos from './components/gestionPagos';
 import Pedidos from './components/pedidos';
 
-
 const App = () => {
   return (
     <Router>
       <div className="app-container">
-        <NavBar />
-        <Routes>
-          <Route path="/Inicio" element={<Inicio />} />
-          <Route path="/Proveedores" element={<Proveedores />} />
-          <Route path="/articulos" element={<Articulos />} />
-          <Route path="/ordenes-de-compra" element={<OrdenesCompra/>} />
-          <Route path="/trabajadores" element={<FormularioTrabajador/>} />
-          <Route path="/Clientes" element={<GestionClientes/>} />
-          <Route path="/Stock" element={<GestionStock/>} />
-          <Route path="/Modelos" element={<FormularioModelo/>} />
-          <Route path="/Pedidos" element={<Pedidos/>} />
-
-          {/* ... otras rutas */}
-        </Routes>
+        <NavBar className="sidebar" />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/Proveedores" element={<Proveedores />} />
+            <Route path="/articulos" element={<Articulos />} />
+            <Route path="/ordenes-de-compra" element={<OrdenesCompra />} />
+            <Route path="/trabajadores" element={<FormularioTrabajador />} />
+            <Route path="/Clientes" element={<GestionClientes />} />
+            <Route path="/Stock" element={<GestionStock />} />
+            <Route path="/Modelos" element={<FormularioModelo />} />
+            <Route path="/Pedidos" element={<Pedidos />} />
+            {/* ... otras rutas */}
+          </Routes>
+        </div>
       </div>
     </Router>
   );
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import '..'
+import '../css/Inicio.css';
 
 const Inicio = () => {
   return (
-    <div className="inicio-container">
-      <h1>Inicio</h1>
-    </div>
+    <section className="about-sec parallax-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <h2>Nosotros</h2>
+          </div>
+          <div className="col-md-4">
+            <h3>¿Quiénes somos?</h3>
+            <p>“Somos una empresa que busca satisfacer la necesidades de nuestros clientes, ofreciendo los mejores productos desde el año 2007, con más de 15 años brindando productos y servicios de calidad y de garantía comprobada”</p>
+          </div>
+          <div className="col-md-4">
+            <h3>Misión</h3>
+            <p>Hacer que el orden y la comodidad sea parte de cualquier espacio propio de nuestros clientes.</p>
+          </div>
+          <div className="col-md-4">
+            <h3>Visión</h3>
+            <p>Convertirnos en un mediano plazo en la primera empresa Boliviana en exportar mobiliario que combina derivados de madera, rejillas y acero, manteniendo estándares de calidad y diseño moderno.</p>
+          </div>
+          <div className="col-md-4">
+            <h3>Historia</h3>
+            <p>2007 : Nace Practicloset legalmente:<br />Nacemos como fabricantes de Rejillas De Acero Plastificadas. Siendo la única empresa a nivel nacional en el rubro.</p>
+            <p>2011 : Ampliamos la unidad de negocio:<br />Empezamos con el diseño y fabricación de mobiliarios utilizando derivados de madera.</p>
+            <p>2015: Combinamos ambos negocios:<br />La tendencia del mercado, generó la idea de la combinación de ambos negocios. Empezamos con el diseño y fabricación de mobiliarios modernos combinando acero y derivados de madera.</p>
+            <p>Hoy en día, continuamos siendo la única empresa a nivel nacional, fabricante de rejillas de acero plastificadas. Habiendo ampliado nuestro mercado a diferentes ciudades de Bolivia. Así mismo nuestros mobiliarios son de gran demanda a nivel nacional.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
